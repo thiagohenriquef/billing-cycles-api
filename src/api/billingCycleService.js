ea0bf6387ec8a0df6dc149777abca1e6e1a985fd
Thiago Henrique Ferreira <thiago.ferreira10@hotmail.com>
2018-11-13 03:21:55 -0200

    fix parameter

--- src/api/billingCycleService.js
+++ src/api/billingCycleService.js
@@ -18,7 +18,7 @@ billingCycle.route('summary', (req, res, next) => {
     { $project: { credit: {$sum: "$credits.value"}, debit: { $sum: "$debits.value" }}},
     { $group: {_id: null, credit: {$sum: "$credit"}, debit: {$sum: "$debit" }}},
     { $project: {_id: 0, credit: 1, debit: 1}},
-    (error, res) => {
+    (error, result) => {
       return error
         ? res.status(500).json({errors: [error]})
         : res.json(result[0] || { credit: 0, debit: 0 })
