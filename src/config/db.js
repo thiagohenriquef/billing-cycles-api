2c449321563bb7a47f509181fbfcc3ab9f2ae7f0
Thiago Henrique Ferreira <thiago.ferreira10@hotmail.com>
2018-11-25 01:24:07 -0200

    heroku scripts

--- src/config/db.js
+++ src/config/db.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
 
+const url = process.env.MONGOLAB_URI ? MONGOLAB_URI : 'mongodb://localhost/payments'
+
 mongoose.Error.messages.general.required = "O atributo '{PATH}' é obrigatório."
 mongoose.Error.messages.Number.min = "O '{VALUE}' informado é menor que o limite mínimo de '{MIN}'."
 mongoose.Error.messages.Number.max = "O '{VALUE}' informado é maior que o limite máximo de '{MAX}'."
 mongoose.Error.messages.String.enum = "'{VALUE}' não é válido para o atributo '{PATH}'."
 
-module.exports = mongoose.connect('mongodb://localhost/payments', { useNewUrlParser: true })
+module.exports = mongoose.connect(url, { useNewUrlParser: true })
