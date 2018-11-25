253e40c9366828bd9a0adb58b92ad733630b06e1
Thiago Henrique Ferreira <thiago.ferreira10@hotmail.com>
2018-11-25 00:30:08 -0200

    updating libs

--- src/api/billingCycle/billingCycleService.js
+++ src/api/billingCycle/billingCycleService.js
@@ -15,15 +15,14 @@ billingCycle.route('count', (req, res, next) => {
 
 billingCycle.route('summary', (req, res, next) => {
   billingCycle.aggregate(
-    { $project: { credit: {$sum: "$credits.value"}, debit: { $sum: "$debits.value" }}},
+    [{ $project: { credit: {$sum: "$credits.value"}, debit: { $sum: "$debits.value" }}},
     { $group: {_id: null, credit: {$sum: "$credit"}, debit: {$sum: "$debit" }}},
-    { $project: {_id: 0, credit: 1, debit: 1}},
-    (error, result) => {
-      return error
-        ? res.status(500).json({errors: [error]})
-        : res.json(result[0] || { credit: 0, debit: 0 })
-    }
-  )
+    { $project: {_id: 0, credit: 1, debit: 1}}]
+  ).exec((error, result) => {
+    return error
+      ? res.status(500).json({errors: [error]})
+      : res.json(result[0] || { credit: 0, debit: 0 })
+  })
 })
 
 
--- src/config/db.js
+++ src/config/db.js
@@ -6,4 +6,4 @@ mongoose.Error.messages.Number.min = "O '{VALUE}' informado é menor que o limit
 mongoose.Error.messages.Number.max = "O '{VALUE}' informado é maior que o limite máximo de '{MAX}'."
 mongoose.Error.messages.String.enum = "'{VALUE}' não é válido para o atributo '{PATH}'."
 
-module.exports = mongoose.connect('mongodb://localhost/payments', { useMongoClient: true })
+module.exports = mongoose.connect('mongodb://localhost/payments', { useNewUrlParser: true })
